f4ba3d73413707600b13c242286f47b91fa2f21c
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2023-08-19 22:59:45 +0530

    Add files via upload

---
+++ February Files/20 Feb 2023/callingfunction.js
@@ -0,0 +1,16 @@
+function greet(){
+    console.log("hi there");
+    console.log("how are you");
+}
+
+
+
+function sqr(x)  {   // x -> parameters 
+    // console.log(x*x);
+    return x*x;
+}
+greet();
+
+
+let a = sqr(8); // 8 -> arguments
+console.log(a);
