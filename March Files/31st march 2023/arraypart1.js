cf0120ce3824cfcc7b453ae73703890dfc4b6c9c
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2023-08-20 23:53:08 +0530

    Add files via upload

---
+++ March Files/31st march 2023/arraypart1.js
@@ -0,0 +1,13 @@
+let names = ["ronak" , "anurag" , "dadwani", "anjali" ]
+// console.log(names);
+// console.log("The length of an Array is : " , names.length);
+
+// console.log(names[names.length - 1]);
+// names[3] = "anjali mehta";
+// console.log(names);
+
+names.push("Apple")
+console.log(names);
+
+names.push("cat")
+console.log(names);
