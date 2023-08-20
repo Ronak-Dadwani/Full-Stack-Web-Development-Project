562ddac2a66991131c725e23fe2f5b0d6bc299df
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2023-08-20 23:52:53 +0530

    Add files via upload

---
+++ March Files/27th March Revision Class/revision.js
@@ -0,0 +1,33 @@
+// Conditions
+
+
+// let star = 1;
+// if (star == 5 ){
+//     console.log ("Excellent Service") ; 
+// }
+// else if  (star == 4)  {
+//     console.log("Good Service");
+// }
+// else if (star == 3 ) {
+//     console.log("Need To Improve");
+// }
+// else{
+//     console.log("I am execting...");
+// }
+
+
+//Loops 
+// 
+// let i = 0 ; 
+
+// for (let i = 0 ; i <= 5 ; i++) {
+//     console.log(i);
+// }
+
+
+// Array 
+
+
+
+
+
